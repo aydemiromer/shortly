f9cdbeaf761cd3b5995a2953e3bb5469d70b8dbc
Omer Erdem Aydemir <aydemirerdemomer@gmail.com>
2021-09-05 12:18:06 +0300

    I Coded extensions for responsive UI

---
+++ lib/src/utils/extensions/context_extension.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+extension ContextExtension on BuildContext {
+  double dynamicWidth(double val) => MediaQuery.of(this).size.width * val;
+  double dynamicHeight(double val) => MediaQuery.of(this).size.height * val;
+}
+
+extension PaddingExtension on BuildContext {
+  EdgeInsets get paddingAllow => EdgeInsets.all(dynamicHeight(0.06));
+
+  EdgeInsets get paddingSymetric =>
+      EdgeInsets.symmetric(vertical: 20, horizontal: 20);
+}
